559fda6f37df38f66570705385787ebf860cc30e
Theophile Champion <tmac3@kent.ac.uk>
2021-01-21 23:18:49 +0000

    Add Empirical Prior

--- examples/learning_maze_navigation.cpp
+++ examples/learning_maze_navigation.cpp
@@ -25,7 +25,9 @@ int main()
     /**
      ** Create the environment and matrix generator.
      **/
-    auto env = std::make_unique<MazeEnv>("../examples/mazes/1.maze");
+    auto maze_file = "../examples/mazes/5.maze";
+    auto evidence_file = "../examples/evidences/5.evi";
+    auto env = std::make_unique<MazeEnv>(maze_file);
 
     /**
      ** Create the model's parameters.
@@ -37,26 +39,6 @@ int main()
     for (int i = 0; i < env->actions(); ++i) {
         theta_B[i] = MatrixXd::Ones(env->states(), env->states());
     }
-
-    /**
-     ** Create the generative model.
-     **/
-    VarNode *U = Dirichlet::create(theta_U);
-    VarNode *A = Dirichlet::create(theta_A);
-    VarNode *B = Dirichlet::create(theta_B);
-    VarNode *D = Dirichlet::create(theta_D);
-    VarNode *a0 = Categorical::create(U);
-    VarNode *s0 = Categorical::create(D);
-    VarNode *o0 = Transition::create(s0, A);
-    o0->setType(VarNodeType::OBSERVED);
-    o0->setName("o0");
-    VarNode *s1 = ActiveTransition::create(s0, a0, B);
-    VarNode *o1 = Transition::create(s1, A);
-    o1->setName("o1");
-    o1->setType(VarNodeType::OBSERVED);
-    std::shared_ptr<FactorGraph> fg = FactorGraph::current();
-    fg->setTreeRoot(s1);
-    fg->loadEvidence(env->observations(), "../examples/evidences/1.evi");
 
     /**
      ** Create the model's prior preferences.
@@ -72,21 +54,64 @@ int main()
      ** Run the simulation.
      **/
     env->print();
-    for (int i = 0; i < 20; ++i) { // Action perception cycle
-        AlgoVMP::inference(fg->getNodes());
-        auto algoTree = std::make_unique<AlgoTree>(env->actions(), D_tilde, E_tilde);
-        for (int j = 0; j < 100; ++j) { // Planning
-            VarNode *n = algoTree->nodeSelection(fg);
-            algoTree->expansion(n, A, B);
-            AlgoVMP::inference(algoTree->lastExpandedNodes());
-            algoTree->evaluation();
-            algoTree->backpropagation(n, fg->treeRoot());
+    for (int i = 0; i < 20; ++i) { // Trials
+        // Reset the environment.
+        std::cout << "Trial number: " << i << std::endl;
+        env = std::make_unique<MazeEnv>(maze_file);
+
+        // Create the generative model.
+        FactorGraph::setCurrent(nullptr);
+        VarNode *U = Dirichlet::create(theta_U);
+        VarNode *A = Dirichlet::create(theta_A);
+        VarNode *B = Dirichlet::create(theta_B);
+        VarNode *D = Dirichlet::create(theta_D);
+        VarNode *a0 = Categorical::create(U);
+        VarNode *s0 = Categorical::create(D);
+        VarNode *o0 = Transition::create(s0, A);
+        o0->setType(VarNodeType::OBSERVED);
+        o0->setName("o0");
+        VarNode *s1 = ActiveTransition::create(s0, a0, B);
+        VarNode *o1 = Transition::create(s1, A);
+        o1->setName("o1");
+        o1->setType(VarNodeType::OBSERVED);
+        std::shared_ptr<FactorGraph> fg = FactorGraph::current();
+        fg->setTreeRoot(s1);
+        fg->loadEvidence(env->observations(), evidence_file);
+
+        for (int j = 0; j < 20; ++j) { // Action perception cycle
+            AlgoVMP::inference(fg->getNodes());
+            auto algoTree = std::make_unique<AlgoTree>(env->actions(), D_tilde, E_tilde);
+            for (int k = 0; k < 50; ++k) { // Planning
+                VarNode *n = algoTree->nodeSelection(fg);
+                algoTree->expansion(n, A, B);
+                AlgoVMP::inference(algoTree->lastExpandedNodes());
+                algoTree->evaluation();
+                algoTree->backpropagation(n, fg->treeRoot());
+            }
+            int a = algoTree->actionSelection(fg->treeRoot());
+            int o = env->execute(a);
+            fg->integrate(a, fg->oneHot(env->observations(), o), A, B);
+            env->print();
         }
-        int a = algoTree->actionSelection(fg->treeRoot());
-        int o = env->execute(a);
-        fg->integrate(a, fg->oneHot(env->observations(), o), A, B);
-        env->print();
+
+        // Performs empirical prior:
+        // i.e. posterior parameters become prior parameters for the next time point
+        theta_U = U->posterior()->params()[0];
+        theta_A = A->posterior()->params()[0];
+        theta_D = D->posterior()->params()[0];
+        for (int ii = 0; ii < env->actions(); ++ii) {
+            theta_B[ii] = B->posterior()->params()[ii];
+        }
+
+        std::cout << "VFE: " << AlgoVMP::vfe(fg->getNodes()) << std::endl;
     }
+
+    std::cout << "U: " << theta_U << std::endl;
+    std::cout << "A: " << theta_A << std::endl;
+    for (int i = 0; i < theta_B.size(); ++i) {
+        std::cout << "B[" << i << "]: " << theta_B[i] << std::endl;
+    }
+    std::cout << "D: " << theta_D << std::endl;
 
     return EXIT_SUCCESS;
 }
